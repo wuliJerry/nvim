09a7d6483332858531eeee328e6dc193f24b9a3a
wuliJerry <wuliJerry927@outlook.com>
2024-01-02 14:51:19 +0800

    add

--- init.lua
+++ init.lua
@@ -681,4 +681,17 @@ vim.opt.mouse  		= 'a'
 vim.keymap.set('n', "<Space>", ":")
 vim.keymap.set('n', "<C-j>", "4j")
 vim.keymap.set('n', "<C-Shift-k>", "4k")
-vim.keymap.set('n', ";;", ":Neotree<CR>")
+vim.api.nvim_set_keymap('n', ';;', ':Neotree<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ';;;', ':Neotree close<CR>', { noremap = true, silent = true })
+if vim.g.neovide then
+  vim.opt.guifont = {"FiraCode Nerd Font Mono", ":h12"}
+  vim.g.neovide_scale_factor = 1.0
+
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+  vim.g.neovide_transparency = 0.9
+  vim.g.transparency = 0.8
+
+  vim.g.neovide_cursor_animation_length = 0.13
+end
